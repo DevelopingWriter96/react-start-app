136496879324508aac7ea8281f7b81b8019ac3dc
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-09-29 14:42:03 -0600

    initial popups

--- src/components/StarShipsCard.js
+++ src/components/StarShipsCard.js
@@ -2,31 +2,39 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import  Typography  from '@mui/material/Typography'
 import CardActions from '@mui/material/CardActions'
-import Button from '@mui/material/Button'
-import './StarShipsCards.css'
+import IconButton from '@mui/material/IconButton'
+import InfoIcon from '@mui/icons-material/Info'
+//import { flexbox } from '@mui/system'
 
-const StarShipsCard = (starships) => {
-    /* const {
-        shipName,
-        shipModel,
-        shipCrew
-    } = props */
+const StarShipsCard = (props) => {
 
+    const handleInfoClick = () => {
+        console.log('click!')
+        props.modalFunction()
+    } 
+    
     return (
-    <Card key={starships.shipCrew} className="card"sx={{
+    <Card 
+        className="card"sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
         width: 168,
-        bgcolor: 'primary.dark',
+        height: 250,
+        m: 1,
         '&:hover': {
           backgroundColor: '#bbb',
           opacity: [0.9, 0.8, 0.7],
         },
     }}>
         <CardContent>
-            <Typography variant="h5" color="success.dark">{starships.name}</Typography>
-            <Typography sx={{ fontSize : 14 }} color="success.dark">{starships.model}</Typography>
+            <Typography variant="h5" color="success.dark">{props.starships.name}</Typography>
+            <Typography sx={{ fontSize : 14 }} color="success.dark">{props.starships.model}</Typography>
         </CardContent>
     <CardActions>
-        <Button size="small">Learn More</Button>
+    <IconButton sx={{ p : 0}} onClick={handleInfoClick}>
+        <InfoIcon />
+    </IconButton>  
     </CardActions>
     </Card>
     )
--- src/components/StarShipsContainer.js
+++ src/components/StarShipsContainer.js
@@ -1,8 +1,27 @@
+import * as React from 'react'
 import { starships } from '../data/starships'
 import  StarShipsCard  from './StarShipsCard'
 import Box from '@mui/material/Box'
+import Modal from '@mui/material/Modal'
+import Typograpy  from '@mui/material/Typography'
+
+const style = {
+    postion: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(75%, 75%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+}
 
 const StarShipsContainer = () => {
+const [open, setOpen] = React.useState(false)
+const handleOpen = () => setOpen(true)
+const handleClose = () => setOpen(false)
+
     return (
         <Box sx={{
             display: "flex",
@@ -10,10 +29,17 @@ const StarShipsContainer = () => {
         }}>
             {starships.map((starships) => {
                 return (
-                    <StarShipsCard {...starships}
+                    <StarShipsCard modalFunction={handleOpen} starships={{...starships}}
                     />
                 )
             })}
+            <Modal open={open} onClose={handleClose}>
+                <Box sx={style}>
+                    <Typograpy variant="h6">
+                        StarShip Information
+                    </Typograpy>
+                </Box>
+            </Modal>
         </Box>
     )
 }
