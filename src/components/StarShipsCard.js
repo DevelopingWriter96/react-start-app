17d830942a0315549b4bb9387550be27ef36e734
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-09-29 15:37:11 -0600

    Color Toggle functional

--- src/components/StarShipsCard.js
+++ src/components/StarShipsCard.js
@@ -1,16 +1,22 @@
+import * as React from 'react'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import  Typography  from '@mui/material/Typography'
 import CardActions from '@mui/material/CardActions'
 import IconButton from '@mui/material/IconButton'
 import InfoIcon from '@mui/icons-material/Info'
+import FavoriteIcon from '@mui/icons-material/Favorite'
 
 const StarShipsCard = (props) => {
-
+    const [favorite, setFavorite] = React.useState(false)
+    
     const handleInfoClick = () => {
-        console.log('click!')
         props.modalFunction()
     } 
+
+    const handleFavoriteClick = () => {
+        setFavorite(favorite ? false : true)
+    }
     
     return (
     <Card 
@@ -33,7 +39,10 @@ const StarShipsCard = (props) => {
     <CardActions>
     <IconButton sx={{ p : 0}} onClick={handleInfoClick}>
         <InfoIcon />
-    </IconButton>  
+    </IconButton>
+    <IconButton sx={{ p: 0, m: 0}} onClick={handleFavoriteClick}>
+        <FavoriteIcon sx={{ color: favorite ? '#F00' : '#000'}}/>
+    </IconButton>      
     </CardActions>
     </Card>
     )
