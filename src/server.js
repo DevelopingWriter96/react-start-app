5924378da4685d4870d152ddcd0ba1e108872f30
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-10-20 19:57:20 -0600

    Moved server.js to source

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const axios = require('axios')
+
+require('dotenv').config()
+
+const app = express()
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    next ()
+})
+
+app.get('/senate', (req, res) => {
+    //axios.get(propublica api goes here)
+    res.json([{name: 'Executor', model: 'Executor-class star dreadnought'}, {name: 'Sentinel-class landing craft', model: 'Sentinel-class landing craft'}])
+})
+
+const PORT = process.env.PORT || 9000
+app.listen(PORT, () => console.log(`Listening on ${PORT}}`))
