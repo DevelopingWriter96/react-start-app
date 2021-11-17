694f762ef84b2685729b1bf820b6f635639247c4
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-11-17 01:21:12 -0700

    Server finally operational

--- src/components/StarShipsContainer.js
+++ src/components/StarShipsContainer.js
@@ -1,86 +1,87 @@
 import * as React from 'react'
 import { starships } from '../data/starships'
-import  StarShipsCard  from './StarShipsCard'
+import StarShipsCard from './StarShipsCard'
 import Box from '@mui/material/Box'
 import Modal from '@mui/material/Modal'
-import { Typography }  from '@mui/material'
+import { Typography } from '@mui/material'
 import axios from 'axios'
 
 const style = {
-    postion: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(75%, 75%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
+  postion: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(75%, 75%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
 }
 
 const StarShipsContainer = () => {
-const [open, setOpen] = React.useState(false)
-const handleOpen = () => setOpen(true)
-const handleClose = () => setOpen(false)
-const [favorites, setFavorites] = React.useState([])
-const [starshipsList, setStarshipsList] = React.useState([])
+  const [open, setOpen] = React.useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+  const [favorites, setFavorites] = React.useState([])
+  const [starshipsList, setStarshipsList] = React.useState([])
 
-React.useEffect(() => {
-  const fetchStarshipList = async () => {
-  console.log("I want to call my api now")
-  try {
-      const response = await axios.get("/starships")
-      console.log(response)
-  } catch (error) {
-    console.log(error)
-  }
-}
-fetchStarshipList()
-}, [])
+  React.useEffect(() => {
+    const fetchStarshipList = async () => {
+      console.log("I want to call my api now")
+      try {
+        const response = await axios.get('/starships')
+        console.log(response)
+        setStarshipsList(response.data) 
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchStarshipList()
+  }, [])
 
-const addToFavorites = (starships) => {
+  const addToFavorites = (starships) => {
     console.log(`${starships.name} was clicked to add to favorites`)
     if (!favorites.includes(starships.name)) {
       setFavorites((prevState) => [...prevState, starships.name])
     } else {
       setFavorites(() => {
         return favorites.filter((item) => item !== starships.name)
-      }) 
+      })
     }
   }
 
-    return (
-        <>
-        <Box>
-            <Typography variant="h4">
+  return (
+    <>
+      <Box>
+        <Typography variant="h4">
 
+        </Typography>
+      </Box>
+      <Box sx={{
+        display: "flex",
+        flexWrap: "wrap"
+      }}>
+
+        {starshipsList.map((starships) => {
+          return (
+            <StarShipsCard
+              key={starships.name}
+              addToFavoritesFunction={addToFavorites}
+              modalFunction={handleOpen}
+              starships={{ ...starships }}
+            />
+          )
+        })}
+        <Modal open={open} onClose={handleClose}>
+          <Box sx={style}>
+            <Typography variant="h6">
+              StarShip Information
             </Typography>
-        </Box>
-        <Box sx={{
-            display: "flex",
-            flexWrap: "wrap"
-        }}>
-
-            {starships.map((starships) => {
-                return (
-                    <StarShipsCard
-                    key={starships.name}
-                    addToFavoritesFunction={addToFavorites} 
-                    modalFunction={handleOpen} 
-                    starships={{...starships}}
-                    />
-                )
-            })}
-            <Modal open={open} onClose={handleClose}>
-                <Box sx={style}>
-                    <Typography variant="h6">
-                        StarShip Information
-                    </Typography>
-                </Box>
-            </Modal>
-        </Box>
-        </>
-    )
+          </Box>
+        </Modal>
+      </Box>
+    </>
+  )
 }
 
 export default StarShipsContainer
--- src/server.js
+++ src/server.js
@@ -7,12 +7,12 @@ const app = express()
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
-    next ()
+    next()
 })
 
 app.get('/starships', (req, res) => {
-    //axios.get(propublica api goes here)
-    res.json([{name: 'Executor', model: 'Executor-class star dreadnought'}, {name: 'Sentinel-class landing craft', model: 'Sentinel-class landing craft'}])
+    //axios.get("zelda api goes here")
+    res.json([{id: 1, name: 'Executor', model: 'Executor-class star dreadnought'}, {id: 2, name: 'Sentinel-class landing craft', model: 'Sentinel-class landing craft'}])
 })
 
 const PORT = process.env.PORT || 9000
