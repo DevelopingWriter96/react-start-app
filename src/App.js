6adb6b230c6591f6ddde991e47b5c444b6ec1698
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-12-11 22:29:15 -0700

    commented out faulty code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import StarShipsContainer from './components/StarShipsContainer'
 import { GamesContextProvider } from './contexts/gamescontext'
+import ButtonAppBar from './components/nav/ButtonAppBar'
 
 function App() {
   return (
     <div className="App">
+      <ButtonAppBar/>
       <GamesContextProvider>
         <StarShipsContainer />
       </GamesContextProvider>
