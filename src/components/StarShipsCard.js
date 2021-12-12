187c6c389f864b86b10962428e7153e4a2478df1
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-12-11 21:24:52 -0700

    testing on netlify

--- src/components/StarShipsCard.js
+++ src/components/StarShipsCard.js
@@ -37,7 +37,7 @@ const StarShipsCard = (props) => {
           //opacity: [0.9, 0.8, 0.7],
         //},
     }}>
-        <CardMedia component='img' alt='Zelda Game Cover' width='325' src={`src/data/zeldacoverart/${props.game._id}.jpg`}/>
+        <CardMedia component='img' alt='Zelda Game Cover' width='325' src={`./zeldacoverart/${props.game._id}.jpg`}/>
         <CardContent>
             <Typography variant="h6" color="success.dark">{props.game.name}</Typography>
             <Typography sx={{ fontSize : 12 }} color="success.dark">{props.game.description}</Typography>
